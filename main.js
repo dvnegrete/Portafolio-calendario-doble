96b6e1297d09abc0e036997866ea17278a5b81a8
Damian <dvnegrete@gmail.com>
2024-06-03 14:16:33 -0600

    Font Family added

--- main.js
+++ main.js
@@ -10,32 +10,43 @@ document.querySelector('#app').innerHTML = `
     <form class="form-input">
       <div>
         <label for="primary-color">--primary-color</label>
-        <input type="color" id="primary-color" name="primary-color" value="#1e5712">
+        <input type="color" id="primary-color" name="primary-color" value="#056dae">
       </div>
       <div>
         <label for="secondary-color">--secondary-color</label>
-        <input type="color" id="secondary-color" name="secondary-color" value="#1b480cf3">
+        <input type="color" id="secondary-color" name="secondary-color" value="#0075c0">
       </div>
       <div>
         <label for="tertiary-color">--tertiary-color</label>
-        <input type="color" id="tertiary-color" name="tertiary-color" value="#6290a1">
+        <input type="color" id="tertiary-color" name="tertiary-color" value="#def6ff">
       </div>
       <div>
         <label for="inactive-color">--inactive-color</label>
-        <input type="color" id="inactive-color" name="inactive-color" value="#737575fa">
+        <input type="color" id="inactive-color" name="inactive-color" value="#B3B3B3">
       </div>
       <div>
         <label for="backgroud-color">--backgroud-color</label>
-        <input type="color" id="backgroud-color" name="backgroud-color" value="#b7d2f1">
+        <input type="color" id="backgroud-color" name="backgroud-color" value="#FFFFFF">
       </div>
       <div>
         <label for="border-color">--border-color</label>
-        <input type="color" id="border-color" name="border-color" value="#cccccc">
+        <input type="color" id="border-color" name="border-color" value="#CCCCCC">
       </div>
       <div>
         <label for="font-color">--font-color</label>
-        <input type="color" id="font-color" name="font-color" value="#171809">
-      </div>
+        <input type="color" id="font-color" name="font-color" value="#323232">
+      </div>      
+      <div>
+      <label for="font-light">--fontlight. Fuente:</label>
+      <select id="font-light" name="font-light">
+          <option value="'InterStateL', sans-serif">InterStateL</option>
+          <option value="'Arial', sans-serif">Arial</option>
+          <option value="'Courier New', monospace">Courier New</option>
+          <option value="'Georgia', serif">Georgia</option>
+          <option value="'Times New Roman', serif">Times New Roman</option>
+          <option value="'Verdana', sans-serif">Verdana</option>
+      </select>
+  </div>
     </form>
 
     <calendar-input-selection/>
@@ -47,6 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const root = document.documentElement;
 
   const colorInputs = document.querySelectorAll('input[type="color"]');
+  const inputFontFamily = document.getElementById('font-light');
 
   colorInputs.forEach(input => {
     input.addEventListener('input', (event) => {
@@ -54,4 +66,11 @@ document.addEventListener("DOMContentLoaded", () => {
       root.style.setProperty(colorVariable, event.target.value);
     });
   });
+
+  inputFontFamily.addEventListener('change', (event) => {
+    const fontFamily = event.target.value;
+    root.style.setProperty('--font-light', fontFamily);
+  });
+
 });
+
